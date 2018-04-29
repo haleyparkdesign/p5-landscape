79bc87f0480adce6e5683193d086389f75ff0b30
hpark <hpark11@wellesley.edu>
2018-04-29 00:09:16 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,71 @@
+var Y_AXIS = 1;
+var X_AXIS = 2;
+var b1, b2, c1, c2;
+var yHigh, yLow, offsetDiff, xoff, c;
+
+var yoff = 0.0; // 2nd dimension of perlin noise
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    c1 = color('rgb(103, 113, 252)');
+    c2 = color('rgb(245, 190, 255)');
+    c3 = color('rgba(0, 61, 190, 0.45)');
+}
+
+function draw() {
+    // Background
+    blendMode(BLEND);
+    setGradient(0, 0, width, height, c1, c2, Y_AXIS);
+    yHigh = 180;
+    yLow = 250;
+    offsetDiff = 0.05;
+    blendMode(MULTIPLY);
+    xoff = yoff * 0.2;
+
+    while (yLow < height) {
+        fill(c3);
+        drawRidge(yHigh, yLow, xoff, offsetDiff);
+        yHigh = yHigh * 1.21 + 10;
+        yLow = yLow * 1.24+ 10
+        xoff += 2;
+        offsetDiff += 0.01;
+    }
+}
+
+function drawRidge(yHigh, yLow, xoff, offsetDiff) {
+    beginShape();
+    noStroke();
+    // Iterate over horizontal pixels
+    for (var x = 0; x <= width; x += 10) {
+        var y = map(noise(xoff), 0, 1, yHigh, yLow);
+        // Set the vertex
+        vertex(x, y);
+        // Increment x dimension for noise
+        xoff += offsetDiff;
+    }
+    // increment y dimension for noise
+    yoff += 0.001;
+    vertex(width, height);
+    vertex(0, height);
+    endShape(CLOSE);
+}
+
+
+function setGradient(x, y, w, h, c1, c2, axis) {
+    noFill();
+    if (axis == Y_AXIS) { // Top to bottom gradient
+        for (var i = y; i <= y + h; i++) {
+            var inter = map(i, y, y + h, 0, 1);
+            var c = lerpColor(c1, c2, inter);
+            stroke(c);
+            line(x, i, x + w, i);
+        }
+    } else if (axis == X_AXIS) { // Left to right gradient
+        for (var i = x; i <= x + w; i++) {
+            var inter = map(i, x, x + w, 0, 1);
+            var c = lerpColor(c1, c2, inter);
+            stroke(c);
+            line(i, y, i, y + h);
+        }
+    }
+}
